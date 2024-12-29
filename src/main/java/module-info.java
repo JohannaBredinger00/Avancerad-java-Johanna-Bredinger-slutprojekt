5a0fd34f86ad8d700a0ef7804bd7dccbe4d17cdc
JohannaBredinger00 <johanna.bredinger@gritacademy.se>
2024-12-29 15:47:47 +0100

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.slutprojektfe {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens org.example.slutprojektfe to javafx.fxml;
-    exports org.example.slutprojektfe;
-}
